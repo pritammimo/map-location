d30ac1cee057517996549b7e586b5f8b6eae27c1
pritam <pritam.saha@webskitters.com>
2021-05-13 12:37:44 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Demo1 from "./map";
+ReactDOM.render(< Demo1/>, document.getElementById("root"));
---
+++ src/map.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker  } from 'google-maps-react';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+class Demo1 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+      latitude:null,
+      longitude:null,
+    };  
+  }
+ 
+  componentDidMount() {
+   if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(
+        position => {
+   
+            this.setState({
+                latitude:position.coords.latitude,
+                longitude:position.coords.longitude
+            })
+         
+
+        }
+        
+    
+      );
+   }
+  }
+  render() {
+    return (
+      <div>
+         
+        <Map
+          google={this.props.google}
+          zoom={18}
+          style={mapStyles}
+          center={{
+            lat: this.state.latitude,
+            lng: this.state.longitude
+          }}
+        >
+         <Marker name={"This is test name"} 
+         position={{ lat: this.state.latitude, lng: this.state.longitude }}/>
+        </Map>
+      </div>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyD_Y1rqX4OYQl9CxhkcX_Bhy0hAs2dysgg')
+})(Demo1);
